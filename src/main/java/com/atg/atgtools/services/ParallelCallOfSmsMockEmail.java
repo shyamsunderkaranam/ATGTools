dd716a9cd84ba96887479bfc716ae399cd175394
shyamsunderkaranam <40713356+shyamsunderkaranam@users.noreply.github.com>
2021-03-30 00:00:53 +0530

    Parallelism added for getting all data scenario

--- src/main/java/com/atg/atgtools/services/ParallelCallOfSmsMockEmail.java
+++ src/main/java/com/atg/atgtools/services/ParallelCallOfSmsMockEmail.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,62 +28,74 @@ public class ParallelCallOfSmsMockEmail {
     PrepareATGLinksService prepareATGLinksService;
 
 
-    //public List<JSONObject> parallelCall(){
     public JSONObject parallelCall(){
 
 
         List<Object> pList = new ArrayList(Arrays.asList(smsFeatureToggleCheck, mockCheckService,emailDummyModeCheckService));
         List<JSONObject> atgLinks = prepareATGLinksService.getAllATGEnvUrls("All");
-        //List<List<JSONObject>> tempList
-        //List<JSONObject> tempJSONObject
+
         JSONObject tempJSONObject = new JSONObject();
+
+
+
+        List<CompletableFuture<JSONObject>> futures = pList.stream()
+                .map(obj -> {
+                    return sharedCall(obj,atgLinks);
+                })
+                .collect(Collectors.toList());
+        List<JSONObject> result =
+                futures.stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.toList())
+                ;
+
+
+        result.forEach(res ->
+        {
+            tempJSONObject.putAll(res);
+        });
+
+
+
+/*
         tempJSONObject.put("SmsFeatureToggleValues",smsFeatureToggleCheck.getData(atgLinks));
         tempJSONObject.put("MockValues",mockCheckService.getData(atgLinks));
         tempJSONObject.put("EmailDummyModeValues",emailDummyModeCheckService.getData(atgLinks));
-/*
-                pList.stream()
-        .map(obj ->
-        {
-
-            if(obj instanceof SmsFeatureToggleCheck) {
-                SmsFeatureToggleCheck tmpObj = (SmsFeatureToggleCheck) obj;
-                //JSONObject tmp = new JSONObject();
-                //tmp.put("SmsFeatureToggleValues",tmpObj.getData(atgLinks));
-                tempJSONObject.put("SmsFeatureToggleValues",tmpObj.getData(atgLinks));
-                //return tmp;
-            }
-            else if(obj instanceof MockCheckService){
-                MockCheckService tmpObj = (MockCheckService) obj;
-                //JSONObject tmp = new JSONObject();
-                tempJSONObject.put("MockValues",tmpObj.getData(atgLinks));
-                //return tmp;
-            }
-            else if(obj instanceof EmailDummyModeCheckService){
-                EmailDummyModeCheckService tmpObj = (EmailDummyModeCheckService) obj;
-                //JSONObject tmp = new JSONObject();
-                tempJSONObject.put("EmailDummyModeValues",tmpObj.getData(atgLinks));
-                //return tmp;
-            }
-
-            return null;
-
-        })
-                        ;
 
  */
-               // .collect(Collectors.toList());
-        /*
-        List<JSONObject> tempJSONObject = new ArrayList<JSONObject>();
-        tempList.stream()
-                .forEach(tmpList -> {
-                    tempJSONObject.addAll(tmpList);
-                });*/
-
-
-
         return tempJSONObject;
 
 
+
+    }
+
+    public CompletableFuture<JSONObject> sharedCall(Object obj, List<JSONObject> atgLinks ){
+
+        CompletableFuture<JSONObject> future = CompletableFuture.supplyAsync(new Supplier<JSONObject>() {
+            @Override
+            public JSONObject get() {
+                if (obj instanceof SmsFeatureToggleCheck) {
+                    SmsFeatureToggleCheck tmpObj = (SmsFeatureToggleCheck) obj;
+                    JSONObject tmp = new JSONObject();
+                    tmp.put("SmsFeatureToggleValues", tmpObj.getData(atgLinks));
+
+                    return tmp;
+                } else if (obj instanceof MockCheckService) {
+                    MockCheckService tmpObj = (MockCheckService) obj;
+                    JSONObject tmp = new JSONObject();
+                    tmp.put("MockValues", tmpObj.getData(atgLinks));
+                    return tmp;
+                } else if (obj instanceof EmailDummyModeCheckService) {
+                    EmailDummyModeCheckService tmpObj = (EmailDummyModeCheckService) obj;
+                    JSONObject tmp = new JSONObject();
+                    tmp.put("EmailDummyModeValues", tmpObj.getData(atgLinks));
+                    return tmp;
+                }
+                return null;
+            }
+
+        });
+        return future;
 
     }
 }
